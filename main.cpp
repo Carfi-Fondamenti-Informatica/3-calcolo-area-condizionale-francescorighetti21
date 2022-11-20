065fa28dddd2cbeafe6ecefc12bf4000f5af625f
francescorighetti21 <117826738+francescorighetti21@users.noreply.github.com>
2022-11-20 16:34:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+float a=0, b=0;
+int opzione=0;
+cin >> a >> b >> opzione;
+switch (opzione){
+    case 0:
+        cout << (a*b)/2<< " " ;
+        break;
+    case 1:
+        cout << a*a << " " ;
+         break;
+    case 2:
+        cout << a*b << " " ;
+break;
+    default:
+        cout << "opzione non valida"<< " ";
+
 }
+ return 0;
+    }
